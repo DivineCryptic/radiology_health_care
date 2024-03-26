ae641d2d76dc05105e687661ec666630ab1d43ce
Divyasimha H R Jois <63149130+Divineleo2001@users.noreply.github.com>
2024-03-26 18:15:15 +0530

    Revert "Hta 16 ranks service units"

--- front_logic/src/inferedTypes.ts
+++
@@ -1,5 +0,0 @@
-import * as z from "zod";
-import { signInSchema, signUpSchema } from "./formSchemas";
-
-export type signInForm = z.infer<typeof signInSchema>;
-export type signUpForm = z.infer<typeof signUpSchema>;
