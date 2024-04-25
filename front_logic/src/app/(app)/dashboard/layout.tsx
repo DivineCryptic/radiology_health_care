73d8a8e669ce37bcbe4e94044280f1c12d88df0b
Divineleo20 <divineleo20@gmail.com>
2024-04-25 17:42:21 +0530

    changes t the metric to come up as side bar from the right has been done

--- front_logic/src/app/(app)/dashboard/layout.tsx
+++ front_logic/src/app/(app)/dashboard/layout.tsx
@@ -7,8 +7,11 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <div lang="en">
-      <div className="px-4">
-        <Navbar>{children}</Navbar>
+      <div className="p-5">
+
+        <Navbar>
+          {children}
+        </Navbar>
       </div>
     </div>
   );
--- front_logic/src/modules/navigation/Navbar.tsx
+++ front_logic/src/modules/navigation/Navbar.tsx
@@ -129,7 +129,7 @@ export function Navbar({ children }: { children?: React.ReactNode }) {
         </div>
       </div>
       <div className="flex flex-col">
-        <header className="flex h-14 items-center gap-4 border-b bg-muted/40  lg:h-[60px] ">
+      <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
           <Sheet>
             <SheetTrigger asChild>
               <Button
